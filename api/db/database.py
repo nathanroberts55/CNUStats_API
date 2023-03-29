fb027b48df6d559d9375c71921caaf3131e84dad
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-03-28 20:27:30 -0400

    Made test data generator for testing Stats API when working on dev locally.

--- api/db/database.py
+++ api/db/database.py
@@ -1,5 +1,7 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, Session, create_engine
 from .models import *
+import random
+from datetime import date
 # UIsing SQLite here but can easily use PostgreSQL by changing the url
 sqlite_file_name = "db.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -13,7 +15,73 @@ engine = create_engine(sqlite_url, echo=True,  connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
     
-
+def create_test_data():
+    session = Session(engine)
+    
+    opponents = [
+        'Mary Washington',
+        'Marymount University',
+        'Virginia Wesleyan University',
+        'University of Mount Union',
+        'Swathmore',
+        'Salisbury University',
+        'York College (PA)',
+        'Lynchburg University'
+        ]
+    
+    for i in range(50):
+        game_date = date.today()
+        rand_team = "Christopher Newport University"
+        rand_opponent = opponents[random.randint(0, 7)]
+        season = "2021-2022"
+        fgm = random.randint(0, 20)
+        fga = random.randint(1, 20)
+        fg_pct = fgm/fga
+        three_fgm = random.randint(0, 10)
+        three_fga = random.randint(1, 10)
+        three_pt_pct = three_fgm/three_fga
+        ftm = random.randint(0, 10)
+        fta = random.randint(1, 10)
+        ft_pct = ftm/fta
+        off_reb = random.randint(0, 7)
+        def_reb = random.randint(0, 7)
+        tot_reb = random.randint(0, 7)
+        pf = random.randint(0, 5)
+        ast = random.randint(0, 7)
+        to = random.randint(0, 7)
+        blk = random.randint(0, 7)
+        stl = random.randint(0, 7)
+        pts = random.randint(0, 45)
+        
+        stat = StatLine(
+            date = game_date,
+            team = rand_team,
+            opponent = rand_opponent,
+            season = season,
+            fgm = fgm,
+            fga = fga,
+            fg_pct = fg_pct,
+            three_fgm = three_fgm,
+            three_fga = three_fga,
+            three_pt_pct = three_pt_pct,
+            ftm = ftm,
+            fta = fta,
+            ft_pct = ft_pct,
+            off_reb = off_reb,
+            def_reb = def_reb,
+            tot_reb = tot_reb,
+            pf = pf,
+            ast = ast,
+            to = to,
+            blk = blk,
+            stl = stl,
+            pts = pts
+        )
+        
+        session.add(stat)
+    session.commit()
+    
+    
 if __name__ == '__main__':
     # creates the table if this file is run independently, as a script
     create_db_and_tables()
